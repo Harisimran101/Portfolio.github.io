399e8b1b94397f48154e51f042c92f3d0c5ef509
Harisimran101 <harisimran1012@gmail.com>
2022-01-08 15:12:30 +0500

    updated

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
-var Scrollbar = window.Scrollbar;
+AOS.init({
+    offset: 280,
+});
 
-Scrollbar.init(document.querySelector('#my-scrollbar'), options);
+var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 130,
+    loop: true,
+    strings: [  
+        '^700Haris', '^500a WebGL Dev', '^500 also Frontend Dev', 
+    ],
+    smartBackspace: true
+});
+
+LottieInteractivity.create({
+mode:"seek",
+player:'#firstLottie',
+container: "#lottie-container",
+actions: [
+{
+visibility:[0,1],
+type: "seek",
+frames: [0, 600],
+},
+]  
+});
